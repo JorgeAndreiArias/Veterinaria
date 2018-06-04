42a693dd3cb1496ec270f79315ccb03979f82ced
Andrei <murdock_andrei@hotmail.com>
2018-06-03 23:23:06 -0500

    3Ra Midifcacion Funcional

---
+++ Controlador/ControladorVenta.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veterinaria.ConnDB;
+using Veterinaria.Modelo;
+
+namespace Veterinaria.Controlador
+{
+    class ControladorVenta
+    {
+        public void InsertSQL(Venta venta, Usuario User, Accesorios acc)
+        {
+            Conexion con = new Conexion();
+            using (SqlCommand cmd = new SqlCommand("sp_AddAccesorio", con.OpenConn()))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter param1 = new SqlParameter("@idAcc", SqlDbType.Int);
+                param1.Value = acc.IdAccesorio;
+                SqlParameter param2 = new SqlParameter("@idUs", SqlDbType.Int);
+                param2.Value = User.IdUsuario;
+                SqlParameter param3 = new SqlParameter("@nCli", SqlDbType.VarChar);
+                param3.Value = venta.NombreCliente;
+                SqlParameter param4 = new SqlParameter("@cant", SqlDbType.Int);
+                param3.Value = venta.Cantidad;
+                SqlParameter param5 = new SqlParameter("@precio", SqlDbType.Float);
+                param3.Value = venta.Precio;
+
+                cmd.Parameters.Add(param1);
+                cmd.Parameters.Add(param2);
+                cmd.Parameters.Add(param3);
+                cmd.Parameters.Add(param4);
+                cmd.Parameters.Add(param5);
+
+                cmd.ExecuteNonQuery();
+            }
+            con.CloseConn();
+        }
+
+        public ObservableCollection<Accesorios> GetAllDates()
+        {
+            ObservableCollection<Accesorios> _list = new ObservableCollection<Accesorios>();
+
+            return _list;
+        }
+    }
+}
---
+++ Modelo/Venta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria.Modelo
+{
+    class Venta
+    {
+        public Usuario User { get; set; }
+        public Accesorios Accesory { get; set; }
+        public string NombreCliente { get; set; }
+        public double Precio { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
